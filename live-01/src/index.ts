14b8fb7746a38c02154464edc07004cc759c8589
Ramon Oliveira <77456126+ramon-cco@users.noreply.github.com>
2022-02-16 15:48:49 -0300

    adcionando o typescript e incrementando a forma de desenvolver

--- live-01/src/index.ts
+++ live-01/src/index.ts
@@ -1,2 +1,14 @@
 
-console.log("Executei ---->")
+
+import express, {Request, Response, NextFunction} from 'express'
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({foo: 'sucesso'})
+});
+
+
+app.listen(3000, () => {
+    console.log(` (-̀ᴗ-́)و ̑̑ --> Aplicação Rodando -U+1F6AA- Porta 3000 <--`)
+})
